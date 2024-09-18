93af21254f5d79c3622e3894a2632decc0d5bb9e
Mani75697 <reachmemani11.8@gmail.com>
2024-09-18 08:54:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python3 /workspaces/Telegram-Bot/Danger.py",
+    "postStartCommand": "python3 /workspaces/Bot/Danger.py",
     "customizations": {
         "vscode": {
             "settings": {
